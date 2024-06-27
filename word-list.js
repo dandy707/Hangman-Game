a5b17f4211cd9d8a35589b7bf4717560115e97cb
Chinedu Chukwuegbim <dandyfrank707@gmail.com>
2024-06-27 11:22:28 +0000

    New Hangman-Game

---
+++ word-list.js
@@ -0,0 +1,96 @@
+const wordList = [
+    {
+        word: "guitar",
+        hint: "A musical instrument with strings"
+    },
+
+    {
+        word: "oxygen",
+        hint: "A colorless, odorless gas essential for life"
+    },
+
+    {
+        word: "mountain",
+        hint: "A large natural elevation of the Earth's surface"
+    },
+
+    {
+        word: "painting",
+        hint: "An art form using colors on a surface to create image or expression"
+    },
+
+    {
+        word: "astronomy",
+        hint: "The scientific study of celestial objects and phenomena"
+    },
+
+    {
+        word: "football",
+        hint: "A popular sports played with spherical ball"
+    },
+
+    {
+        word: "dance",
+        hint: "A rhythmic movement of the body performed to music."
+    },
+
+    {
+        word: "science",
+        hint: "The systematic study of the structure and behavior of the physical and "
+    },
+
+    {
+        word: "sunset",
+        hint: "The daily disappearance of the sun below the horizon."
+    },
+
+    {
+        word: "coffee",
+        hint: "A popular caffeinated beverage made from roasted coffee bean."
+    },
+
+    {
+        word: "bicycle",
+        hint: "A human-powered vehicle with two wheels."
+    },
+
+    {
+        word: "camera",
+        hint: "A device used to capture amd record images or videos."
+    },
+
+    {
+        word: "adventure",
+        hint: "An exciting or daring experience."
+    },
+
+    {
+        word: "diamond",
+        hint: "A precious gemstone known for its brilliance and hardness."
+    },
+
+    {
+        word: "jazz",
+        hint: "A genre of music characterized by improvisation and syncopation"
+    },
+
+    {
+        word: "pizza",
+        hint: "A savory dish consisting of a round, flattened base with toppings. "
+    },
+
+    {
+        word: "history",
+        hint: "The study of past events and human civilization."
+    },
+
+    {
+        word: "butterfly",
+        hint: "An insect with colorful wings and a slender body."
+    },
+    {
+        word: "chocolate",
+        hint: "A sweet treat made from cocoa beans."
+    }
+    
+];
